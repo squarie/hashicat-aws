cd848babe686b623e19588cec635e1153637e710
Sarah Johnson <srhjohn@amazon.com>
2023-08-24 16:45:58 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/skwarie/s3-bucket/aws"
   bucket_prefix = "sarahjohnson"
-  tags = "Department", "Billable"
+  tags = "Department"
+  tags = "Billable"
   version = "2.8.0"
 }
